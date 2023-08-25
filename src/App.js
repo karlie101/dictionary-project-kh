348e571b88e62be8fe7d688bcdc52c1e82f51d9a
karlie101 <129705716+karlie101@users.noreply.github.com>
2023-08-25 00:12:23 -0500

    initial comment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import dictionary from './dictionary-logo.png';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <img src={dictionary} className="App-dictionary-logo img-fluid"
+        alt="dictionary" />
+
+      </header>
+    </div>
+  );
+}
+
+
