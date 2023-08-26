8354be1c639c05606e53e779d340ed213005c8a4
karlie101 <129705716+karlie101@users.noreply.github.com>
2023-08-26 15:53:37 -0500

    Added the search engine of the dictionary.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import dictionary from './dictionary-logo.png';
-import './App.css';
+import logo from "./dictionary-logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <img src={dictionary} className="App-dictionary-logo img-fluid"
+      <div className="container">
+         <header className="App-header">
+           <img src={logo} className="App-dictionary-logo img-fluid"
         alt="dictionary" />
-
-      </header>
+         </header>
+         <main>
+          <Dictionary />
+         </main>
+         <footer className="App-footer">
+            <small>Coded by Karlie</small>
+         </footer>
+      </div>
     </div>
   );
 }
